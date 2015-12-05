5de611f672393626b2ec6ca179fbaa7a7d2a333d
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-05 01:16:12 +0100

    fix wmic not returning valid CSV on Windows
    The CommandLine column contained `,` (which is the separator) while not being quoted.
    
    I hate Windows so much I want to cry...
    
    Fixes #10

--- win.js
+++ win.js
@@ -1,19 +1,19 @@
 'use strict';
 const childProcess = require('child_process');
-const neatCsv = require('neat-csv');
 const pify = require('pify');
+const execall = require('execall');
 const TEN_MEBIBYTE = 1024 * 1024 * 10;
 
 module.exports = opts => {
 	opts = opts || {};
 
-	const cmd = `wmic process where Caption='chrome.exe' get CommandLine,ProcessId /format:csv`;
+	const cmd = `wmic process where Caption='chrome.exe' get CommandLine,ProcessId /format:list`;
 
-	return pify(childProcess.exec)(cmd, {maxBuffer: TEN_MEBIBYTE}).then(
-		stdout => pify(neatCsv)(stdout.trim()).then(
-			data => data
-				.filter(x => x.Node !== undefined)
-				.map(x => ({cmd: x.CommandLine, pid: parseInt(x.ProcessId, 10)}))
-		)
-	);
+	return pify(childProcess.exec)(cmd, {maxBuffer: TEN_MEBIBYTE})
+		.then(stdout =>
+			execall(/CommandLine=(.+)\s+ProcessId=(\d+)/g, stdout).map(x => ({
+				cmd: x.sub[0],
+				pid: parseInt(x.sub[1], 10)
+			}))
+		);
 };
