62192c4c56bdaa222290682173de9a537d3834a1
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-05 18:52:58 +0700

    make XO happy

--- win.js
+++ win.js
@@ -2,11 +2,10 @@
 const childProcess = require('child_process');
 const pify = require('pify');
 const execall = require('execall');
+
 const TEN_MEBIBYTE = 1024 * 1024 * 10;
 
-module.exports = opts => {
-	opts = opts || {};
-
+module.exports = () => {
 	const cmd = `wmic process where Caption='chrome.exe' get CommandLine,ProcessId /format:list`;
 
 	return pify(childProcess.exec)(cmd, {maxBuffer: TEN_MEBIBYTE})
