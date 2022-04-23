5846e0fd3bbb12c233e218be853349c6fbc0e4ca
GeorgeGkas <georgegkas@gmail.com>
2022-04-23 17:50:44 +0300

    Cannot get available browser processes on Windows (#22)

--- windows.js
+++ windows.js
@@ -6,9 +6,14 @@ const TEN_MEBIBYTE = 1024 * 1024 * 10;
 
 export default async function killTabs() {
 	const command = 'wmic process where Caption=\'chrome.exe\' get CommandLine,ProcessId /format:list';
-	const stdout = await promisify(childProcess.exec)(command, {maxBuffer: TEN_MEBIBYTE});
+	const res = await promisify(childProcess.exec)(command, {maxBuffer: TEN_MEBIBYTE});
 
-	return execall(/CommandLine=(.+)\s+ProcessId=(\d+)/g, stdout).map(element => ({
+	if (res.stderr) {
+		console.error(res.stderr)
+		process.exit(1)
+	}
+
+	return execall(/CommandLine=(.+)\s+ProcessId=(\d+)/g, res.stdout).map(element => ({
 		cmd: element.subMatches[0],
 		pid: Number.parseInt(element.subMatches[1], 10),
 	}));
